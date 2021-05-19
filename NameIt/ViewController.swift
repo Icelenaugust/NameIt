b8d96eb0fb0ef9009b758a46d77369290528e91d
Icelenaugust <dingyoujia0823@gmail.com>
2021-05-19 21:11:46 +0800

    Commit

--- NameIt/ViewController.swift
+++ NameIt/ViewController.swift
@@ -5,15 +5,69 @@
 //  Created by Youjia Ding on 19/5/21.
 //
 
+import CoreML
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+                      UIImagePickerControllerDelegate,
+                      UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var frontImage: UIImageView!
+    
+    @IBOutlet weak var objectName: UILabel!
+    
+    @IBOutlet weak var selectImage: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func chooseImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+        print("Select image button pressed")
+        
+    }
+    
+    private func analyzeImage(image: UIImage?) {
+        guard let buffer = image?.resize(size: CGSize(width: 224, height: 224))?.getCVPixelBuffer() else {
+            return
+        }
+        
+        do {
+            let config = MLModelConfiguration()
+            let model = try GoogLeNetPlaces(configuration: config)
+            let input = GoogLeNetPlacesInput(sceneImage: buffer)
+            
+            let output = try model.prediction(input: input)
+            let nameString = output.sceneLabel
+            objectName.text = nameString
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey : Any]) {
+        
+        guard let frontPicture = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        
+        frontImage.image = frontPicture
+        analyzeImage(image: frontPicture)
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
